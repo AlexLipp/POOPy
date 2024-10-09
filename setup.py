36491efea510162ce0db54472caa7daa5c1dd5b3
Alex Lipp <alexander.lipp@merton.ox.ac.uk>
2024-10-09 16:07:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ extensions = [
 
 setup(
     name="poopy",
-    version="0.2",
+    version="0.2.1",
     ext_modules=cythonize(extensions),
     include_dirs=[numpy.get_include()],
     packages=find_packages(),
