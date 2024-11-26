9e212278395bfb098103092c5f00eff69379f514
Alex Lipp <alex@lipp.org.uk>
2024-11-26 16:57:59 +0000

    Add UnitedUtilities class

--- poopy/companies.py
+++ poopy/companies.py
@@ -1077,7 +1077,6 @@ class SouthWestWater(WaterCompany):
             event = NoDischarge(
                 monitor=monitor,
                 ongoing=True,
-                # Assume the the period of no discharge is from the end of the last event to the current time
                 start_time=pd.to_datetime(row["statusStart"], unit="ms"),
             )
         elif row["status"] == -1:
@@ -1090,6 +1089,140 @@ class SouthWestWater(WaterCompany):
             # Raise an exception if the status is not -1, 0 or 1 (should not happen!)
             raise Exception(
                 "Unknown status type " + row["status"] + " for monitor " + row["ID"]
+            )
+        return event
+    
+class UnitedUtilities(WaterCompany):
+    """
+    Creates an object to interact with the United Utilities EDM API.
+    There is no auth on this endpoint required currently.
+    There is only a current status endpoint, no historical endpoint available.
+    """
+
+    API_ROOT = "https://services5.arcgis.com/5eoLvR0f8HKb7HWP/arcgis/rest/services/"
+    CURRENT_API_RESOURCE = (
+        "United_Utilities_Storm_Overflow_Activity/FeatureServer/0/query"
+    )
+    HISTORICAL_API_RESOURCE = ""
+    API_LIMIT = 2000  # Max num of outputs that can be requested from the API at once
+
+    # D8_FILE_URL = "https://zenodo.org/records/14219156/files/southern_d8.nc?download=1"
+    # D8_FILE_HASH = "md5:4696dfce4e1c4cdc0479af03e6b38106"
+
+    def __init__(self, clientID="", clientSecret=""):
+        # No auth required for this API so no need to pass in clientID and clientSecret
+        print("\033[36m" + "Initialising United Utilities object..." + "\033[0m")
+        self._name = "United Utilities"
+        super().__init__(clientID, clientSecret)
+        # self._d8_file_path = self._fetch_d8_file(
+        #     url=self.D8_FILE_URL,
+        #     known_hash=self.D8_FILE_HASH,
+        # )
+        self._alerts_table = f"{self._name}_alerts.csv"
+        self._alerts_table_update_list = f"{self._name}_alerts_update_list.dat"
+
+    def _fetch_monitor_history(self, monitor: Monitor) -> List[Event]:
+        """
+        Not available for United Utilities API.
+        """
+        print(
+            "\033[36m"
+            + "This function is not available for the United Utilities API."
+            + "\033[0m"
+        )
+        pass
+        return
+
+    def set_all_histories(self) -> None:
+        """
+        Not available for United Utilities API.
+        """
+        print(
+            "\033[36m"
+            + "This function is not available for the United Utilities API."
+            + "\033[0m"
+        )
+        pass
+        return
+
+    def _row_to_monitor(self, row: pd.DataFrame) -> Monitor:
+        """
+        Convert a row of the United Utilities active API response to a Monitor object. See `_fetch_current_status_df`
+        """
+        current_time = (
+            self._timestamp
+        )  # Get the current time which corresponds to when the API was called (so it is same for all monitors)
+
+        x, y = latlong_to_osgb(row["Latitude"], row["Longitude"])
+
+        # if monitor currently discharging we set last_48h to be True.
+        if row["Status"] == 1:
+            last_48h = True
+
+        elif pd.notnull(row["LatestEventEnd"]):
+            # if monitor has different status (i.e., offline or not discharging) but has discharged in the last 48 hours we also set last_48h to be True.
+            last_48h = (
+                current_time - pd.to_datetime(row["LatestEventEnd"], unit="ms")
+            ) <= timedelta(hours=48)
+        else:
+            # This is normally the case when the monitor has never discharged. But for UU the information in the data-stream is not clear, 
+            # specifically the "LatestEventEnd" field is not always populated sensibly (nor is the "StatusStart" field). Shrug!
+            last_48h = None
+
+        # Parse row["ReceivingWaterCourse"] to a string, including when it is None
+        if pd.isna(row["ReceivingWaterCourse"]):
+            receiving_watercourse = "Unknown"
+        else:
+            receiving_watercourse = row["ReceivingWaterCourse"]
+
+        return Monitor(
+            site_name=row["Id"],  # United Utilities does not provide a site name
+            permit_number="Unknown",  # Assuming that the permit number is the ID
+            x_coord=x,
+            y_coord=y,
+            receiving_watercourse=receiving_watercourse,
+            water_company=self,
+            discharge_in_last_48h=last_48h,
+        )
+
+    def _row_to_event(self, row: pd.DataFrame, monitor: Monitor) -> Event:
+        """
+        Convert a row of the United Utilities active API response to an Event object. See `_fetch_current_status_df`
+        """
+        # If event_end is NaT update event_end to be None. This is normally because the monitor is yet
+        # to have a discharge event. So, cannot sensibly record the start time of the no discharge event.
+        # if row["latestEventEnd"] is not nan, convert it to datetime, else set it to Non
+
+        if row["Status"] == 1:
+            event = Discharge(
+                monitor=monitor,
+                ongoing=True,
+                start_time=pd.to_datetime(row["LatestEventStart"], unit="ms"),
+            )
+
+        elif row["Status"] == 0:
+            if not pd.isna(row["LatestEventEnd"]):
+                last_event_end = pd.to_datetime(row["LatestEventEnd"], unit="ms")
+            else:
+                # UU datastream seems to not provide the end time of the last event seemingly arbitrarily?
+                last_event_end = None
+            event = NoDischarge(
+                monitor=monitor,
+                ongoing=True,
+                # Assume the the period of no discharge is from the end of the last event to the current time
+                start_time=last_event_end,
+            )
+        elif row["Status"] == -1:
+            event = Offline(
+                monitor=monitor,
+                ongoing=True,
+                # UU data-stream does not seem to sensibly utilise the "StatusStart" field and so cannot be used to determine the start time of the offline event!
+                start_time=None,
+            )
+        else:
+            # Raise an exception if the status is not -1, 0 or 1 (should not happen!)
+            raise Exception(
+                "Unknown status type " + row["Status"] + " for monitor " + row["Id"]
             )
         return event
 
