4e84b0abae666d68ba2917c6eb3fff623747c98c
Alex Lipp <alex@lipp.org.uk>
2024-11-26 14:43:57 +0000

    Add SouthWest Water class

--- poopy/companies.py
+++ poopy/companies.py
@@ -973,6 +973,127 @@ class WessexWater(WaterCompany):
         return event
 
 
+class SouthWestWater(WaterCompany):
+    """
+    Creates an object to interact with the South West Water EDM API.
+    There is no auth on this endpoint required currently.
+    There is only a current status endpoint, no historical endpoint available.
+    """
+
+    API_ROOT = "https://services-eu1.arcgis.com/OMdMOtfhATJPcHe3/arcgis/rest/services/"
+    CURRENT_API_RESOURCE = "NEH_outlets_PROD/FeatureServer/0/query"
+    HISTORICAL_API_RESOURCE = ""
+    API_LIMIT = 1000  # Max num of outputs that can be requested from the API at once
+
+    # D8_FILE_URL = "https://zenodo.org/records/14219156/files/southern_d8.nc?download=1"
+    # D8_FILE_HASH = "md5:4696dfce4e1c4cdc0479af03e6b38106"
+
+    def __init__(self, clientID="", clientSecret=""):
+        # No auth required for this API so no need to pass in clientID and clientSecret
+        print("\033[36m" + "Initialising South West Water object..." + "\033[0m")
+        self._name = "SouthWest Water"
+        super().__init__(clientID, clientSecret)
+        # self._d8_file_path = self._fetch_d8_file(
+        #     url=self.D8_FILE_URL,
+        #     known_hash=self.D8_FILE_HASH,
+        # )
+        self._alerts_table = f"{self._name}_alerts.csv"
+        self._alerts_table_update_list = f"{self._name}_alerts_update_list.dat"
+
+    def _fetch_monitor_history(self, monitor: Monitor) -> List[Event]:
+        """
+        Not available for South West Water API.
+        """
+        print(
+            "\033[36m"
+            + "This function is not available for the South West Water API."
+            + "\033[0m"
+        )
+        pass
+        return
+
+    def set_all_histories(self) -> None:
+        """
+        Not available for South West Water API.
+        """
+        print(
+            "\033[36m"
+            + "This function is not available for the South West Water API."
+            + "\033[0m"
+        )
+        pass
+        return
+
+    def _row_to_monitor(self, row: pd.DataFrame) -> Monitor:
+        """
+        Convert a row of the South West Water active API response to a Monitor object. See `_fetch_current_status_df`
+        """
+        current_time = (
+            self._timestamp
+        )  # Get the current time which corresponds to when the API was called (so it is same for all monitors)
+
+        x, y = latlong_to_osgb(row["latitude"], row["longitude"])
+
+        # if monitor currently discharging we set last_48h to be True.
+        if row["status"] == 1:
+            last_48h = True
+
+        elif pd.notnull(row["latestEventEnd"]):
+            # if monitor has different status (i.e., offline or not discharging) but has discharged in the last 48 hours we also set last_48h to be True.
+            last_48h = (
+                current_time - pd.to_datetime(row["latestEventEnd"], unit="ms")
+            ) <= timedelta(hours=48)
+        else:
+            # This is normally the case when the monitor has never discharged
+            last_48h = None
+
+        # Parse row["ReceivingWaterCourse"] to a string, including when it is None
+        if pd.isna(row["receivingWaterCourse"]):
+            receiving_watercourse = "Unknown"
+        else:
+            receiving_watercourse = row["receivingWaterCourse"]
+
+        return Monitor(
+            site_name=row["ID"],  # South West Water does not provide a site name
+            permit_number="Unknown",  # Assuming that the permit number is the ID
+            x_coord=x,
+            y_coord=y,
+            receiving_watercourse=receiving_watercourse,
+            water_company=self,
+            discharge_in_last_48h=last_48h,
+        )
+
+    def _row_to_event(self, row: pd.DataFrame, monitor: Monitor) -> Event:
+        """
+        Convert a row of the South West Water active API response to an Event object. See `_fetch_current_status_df`
+        """
+        if row["status"] == 1:
+            event = Discharge(
+                monitor=monitor,
+                ongoing=True,
+                start_time=pd.to_datetime(row["statusStart"], unit="ms"),
+            )
+        elif row["status"] == 0:
+            event = NoDischarge(
+                monitor=monitor,
+                ongoing=True,
+                # Assume the the period of no discharge is from the end of the last event to the current time
+                start_time=pd.to_datetime(row["statusStart"], unit="ms"),
+            )
+        elif row["status"] == -1:
+            event = Offline(
+                monitor=monitor,
+                ongoing=True,
+                start_time=pd.to_datetime(row["statusStart"], unit="ms"),
+            )
+        else:
+            # Raise an exception if the status is not -1, 0 or 1 (should not happen!)
+            raise Exception(
+                "Unknown status type " + row["status"] + " for monitor " + row["ID"]
+            )
+        return event
+
+
 def latlong_to_osgb(lat, lon):
     # Define the WGS84 spatial reference system
     wgs84 = osr.SpatialReference()
